ae433c95b1bf735e2a345ab9dee49f8697cf749b
AnasGherfal <79544599+AnasGherfal@users.noreply.github.com>
2022-05-11 01:58:56 -0700

    Changed the position of chat and contacts. Added what the password should contain when registering. Also added the seething and theme, but still not functional.

--- app/src/main/java/edu/uw/tcss450/group6App/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group6App/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.uw.tcss450.group6App;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,10 +21,17 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.uw.tcss450.group6App.model.UserInfoViewModel;
+import edu.uw.tcss450.group6App.ui.home.HomeFragment;
+import edu.uw.tcss450.group6App.ui.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     private AppBarConfiguration mAppBarConfiguration;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         // Make sure the new statements go BELOW setContentView
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -64,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_settings) {
             //TODO open a settings fragment
+
+            getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).replace(R.id.container, new SettingsFragment()).commit();
+            //mAppBarConfiguration
             Log.d("SETTINGS", "Clicked");
             return true;
         }
