90e8ab403cfb0c1947b08833f24298b808ad5daa
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-13 16:25:13 -0700

    Implemented a basic GET, next I will GET data from weatherbit.io

--- app/src/main/java/edu/uw/tcss450/group6App/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/group6App/ui/weather/WeatherFragment.java
@@ -2,13 +2,19 @@ package edu.uw.tcss450.group6App.ui.weather;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.group6App.R;
+import edu.uw.tcss450.group6App.databinding.FragmentContactSearchBinding;
+import edu.uw.tcss450.group6App.databinding.FragmentWeatherBinding;
+import edu.uw.tcss450.group6App.ui.contacts.ContactsViewModel;
 
 /**
  * Contains all weather related functionality.
@@ -18,6 +24,15 @@ import edu.uw.tcss450.group6App.R;
  * create an instance of this fragment.
  */
 public class WeatherFragment extends Fragment {
+    /**
+     * API key from weatherbit.io from Robert's UW email. TODO might have to go in WeatherViewModel.java
+     */
+    private static final String apiKey = "d76b1605bb534d7ea82ace219ff60b96";
+
+
+    private WeatherViewModel mViewModel;
+
+    private FragmentWeatherBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,12 +72,26 @@ public class WeatherFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mViewModel = new ViewModelProvider(getActivity())
+                .get(WeatherViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        //return inflater.inflate(R.layout.fragment_weather, container, false);
+        binding = FragmentWeatherBinding.inflate(inflater);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel.addResponseObserver(getViewLifecycleOwner(), result ->
+                binding.textResponseOutput.setText(result.toString()));
+        binding.todayButton.setOnClickListener(button -> mViewModel.connectGet());
+    }
+
+
 }
